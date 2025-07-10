fdfc056c5a531e6372227e86ac12af4facf1e6f5
pochun840 <0840@kilews.com.tw>
2025-07-10 10:46:43 +0800

    修改 creat_seq 時 act的預設值

--- app/controllers/Sequences.php
+++ app/controllers/Sequences.php
@@ -85,7 +85,7 @@ class Sequences extends Controller
                 'SEQname' => $_POST['SEQname'] ?? null,
                 'time' => $_POST['time'] ?? null,
                 'type' => $_POST['type'] ?? null,
-                'act' => $_POST['act'] ?? 0,
+                'act' => $_POST['act'] ?? 1,
                 'skip' => $_POST['skip'] ?? null,
                 'seq_repeat' => $_POST['seq_repeat'] ?? null,
                 'timeout' => $_POST['timeout'] ?? null,
@@ -195,8 +195,8 @@ class Sequences extends Controller
 
         $data = array(
             'seq_info' => $res,
-            'job_id' => $job_id,
-            'seq_id' => $seq_id
+            'job_id' => $jobid,
+            'seq_id' => $seqid
         );
     }
 
